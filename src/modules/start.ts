ed4a1b5a3beb115f54d03d65d4991dee24eddffd
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2023-06-30 08:03:32 +0700

    Update start.ts

--- src/modules/start.ts
+++ src/modules/start.ts
@@ -428,7 +428,7 @@ export async function kick(ctx) {
       );
     } else {
     let replyMsg = ctx.message.reply_to_message
-    const status = await bot.telegram.getChatMember(ctx.chat.id, ctx.from.id);
+    const status = await ctx.telegram.getChatMember(ctx.chat.id, ctx.from.id);
      if (status == "member" || status == "user"){
       return ctx.replyWithHTML(`<b>Kamu harus jadi admin atau owner grup untuk melakukan tindakan ini.</b>\n⏱ <code>${c}</code> | ⏳ <code>${await getPing(ctx)}</code>`)
     }
