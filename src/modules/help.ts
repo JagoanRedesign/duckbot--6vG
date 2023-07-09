be0082607e12d3d4e493c970ea5a016fbc4f86a8
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2023-07-09 19:12:35 +0700

    Update help.ts

--- src/modules/help.ts
+++ src/modules/help.ts
@@ -20,10 +20,21 @@ export async function helpmenu(ctx) {
     let keyboard = [
       [
         {
-          text: 'admin',
+          text: 'Admin',
           callback_data: 'menuadmin',
           hide: true,
         },
+       {
+        text: `Filter`,
+        callback_data: 'menufilter',
+        hide: true,
+      },
+      {
+        text: `Note`,
+        callback_data: 'menunote',
+        hide: true,
+      },
+
       ],
     ]
     
@@ -34,6 +45,7 @@ export async function helpmenu(ctx) {
         reply_markup: {
           inline_keyboard: keyboard,
         },
+        parse_mode: 'HTML',
       }
     )
   } catch (error) {
@@ -47,14 +59,13 @@ export async function helpmenu(ctx) {
 
 
 export async function menuadmin(ctx) {
-  let c = await getPing(ctx)
+ 
   try {
-    let langs = await getLang(ctx)
     
     let keyboard = [
       [
         {
-          text: 'Kembali',
+          text: 'ᴋᴇᴍʙᴀʟɪ ➣',
           callback_data: 'helpmenu',
           hide: true,
         },
@@ -63,11 +74,12 @@ export async function menuadmin(ctx) {
     
     
     return ctx.editMessageText(
-      `<b>Admin</b>\n\nPerintah yang Tersedia:\n<code>/admincache</code> - menyegarkan cache dari daftar admin.\n<code>/settings</code> - dapatkan pengaturan grup saat ini`,
+      `<b>Admin</b>\nPerintah yang Tersedia:\n\n<code>/admincache</code> - menyegarkan cache dari daftar admin.\n<code>/settings</code> - dapatkan pengaturan grup saat ini`,
       {
         reply_markup: {
           inline_keyboard: keyboard,
         },
+        parse_mode: 'HTML',
       }
     )
   } catch (error) {
@@ -75,3 +87,63 @@ export async function menuadmin(ctx) {
   }
 }
 
+
+export async function menufilter(ctx) {
+ 
+  try {
+    
+    let keyboard = [
+      [
+        {
+          text: 'ᴋᴇᴍʙᴀʟɪ ➣',
+          callback_data: 'helpmenu',
+          hide: true,
+        },
+      ],
+    ]
+    
+    
+    return ctx.editMessageText(
+      `<b>Filter</b>\nPerintah yang Tersedia :\n\n<code>/filter</code> <b>{filterName} {filterValue}</b> - simpan catatan baru atau perbarui filter.\nUntuk <b>{filterValue}</b>bagian tersebut anda bisa menggantinya dengan membalas pesan.\n<code>/stop</code> <b>{filterName}</b> - hapus beberapa filter\n<code>/clearall</code> - hapus semua catatan\n<code>/filters </code>- dapatkan daftar semua filter`,
+      {
+        reply_markup: {
+          inline_keyboard: keyboard,
+        },
+        parse_mode: 'HTML',
+      }
+    )
+  } catch (error) {
+    return reportError(error, ctx)
+  }
+}
+
+
+
+export async function menunote(ctx) {
+ 
+  try {
+    
+    let keyboard = [
+      [
+        {
+          text: 'ᴋᴇᴍʙᴀʟɪ ➣',
+          callback_data: 'helpmenu',
+          hide: true,
+        },
+      ],
+    ]
+    
+    
+    return ctx.editMessageText(
+      `<b>Notes</b>\nPerintah yang Tersedia :\n\n<code>/save</code> <b>{noteName} {noteValue}</b> - simpan catatan baru atau perbarui catatan.\nUntuk <b>{noteValue}</b>bagian tersebut anda bisa menggantinya dengan membalas pesan.\n<code>/clear</code> <b>{noteName}</b> - hapus beberapa catatan\n<code>/clearall</code> - hapus semua catatan\n<code>/notes</code> - dapatkan daftar semua catatan`,
+      {
+        reply_markup: {
+          inline_keyboard: keyboard,
+        },
+        parse_mode: 'HTML',
+      }
+    )
+  } catch (error) {
+    return reportError(error, ctx)
+  }
+}
