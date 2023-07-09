17d8d7d4372bebc665a93c965402d38c85915c4c
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2023-07-09 20:11:13 +0700

    Update help.ts

--- src/modules/help.ts
+++ src/modules/help.ts
@@ -36,6 +36,24 @@ export async function helpmenu(ctx) {
       },
 
       ],
+      [
+        {
+          text: 'Purge',
+          callback_data: 'menupurge',
+          hide: true,
+        },
+       {
+        text: `Report`,
+        callback_data: 'menureport',
+        hide: true,
+      },
+      {
+        text: `Special`,
+        callback_data: 'menuspecial',
+        hide: true,
+      },
+
+      ],
     ]
     
     
@@ -65,7 +83,7 @@ export async function menuadmin(ctx) {
     let keyboard = [
       [
         {
-          text: 'ᴋᴇᴍʙᴀʟɪ ➣',
+          text: '⬅ ᴋᴇᴍʙᴀʟɪ',
           callback_data: 'helpmenu',
           hide: true,
         },
@@ -74,7 +92,7 @@ export async function menuadmin(ctx) {
     
     
     return ctx.editMessageText(
-      `<b>Admin</b>\nPerintah yang Tersedia:\n\n<code>/admincache</code> - menyegarkan cache dari daftar admin.\n<code>/settings</code> - dapatkan pengaturan grup saat ini`,
+      `Perintah yang Tersedia untuk <b>Admin</b>:\n\n<code>/admincache</code> - menyegarkan cache dari daftar admin.\n<code>/settings</code> - dapatkan pengaturan grup saat ini`,
       {
         reply_markup: {
           inline_keyboard: keyboard,
@@ -95,7 +113,7 @@ export async function menufilter(ctx) {
     let keyboard = [
       [
         {
-          text: 'ᴋᴇᴍʙᴀʟɪ ➣',
+          text: '⬅ ᴋᴇᴍʙᴀʟɪ',
           callback_data: 'helpmenu',
           hide: true,
         },
@@ -104,7 +122,7 @@ export async function menufilter(ctx) {
     
     
     return ctx.editMessageText(
-      `<b>Filter</b>\nPerintah yang Tersedia :\n\n<code>/filter</code> <b>{filterName} {filterValue}</b> - simpan catatan baru atau perbarui filter.\nUntuk <b>{filterValue}</b>bagian tersebut anda bisa menggantinya dengan membalas pesan.\n<code>/stop</code> <b>{filterName}</b> - hapus beberapa filter\n<code>/clearall</code> - hapus semua catatan\n<code>/filters </code>- dapatkan daftar semua filter`,
+      `Perintah yang Tersedia untuk <b>Filter</b>:\n\n<code>/filter</code> <b>{filterName} {filterValue}</b> - simpan catatan baru atau perbarui filter.\nUntuk <b>{filterValue}</b>bagian tersebut anda bisa menggantinya dengan membalas pesan.\n<code>/stop</code> <b>{filterName}</b> - hapus beberapa filter\n<code>/clearall</code> - hapus semua catatan\n<code>/filters </code>- dapatkan daftar semua filter`,
       {
         reply_markup: {
           inline_keyboard: keyboard,
@@ -126,7 +144,7 @@ export async function menunote(ctx) {
     let keyboard = [
       [
         {
-          text: 'ᴋᴇᴍʙᴀʟɪ ➣',
+          text: '⬅ ᴋᴇᴍʙᴀʟɪ',
           callback_data: 'helpmenu',
           hide: true,
         },
@@ -135,7 +153,96 @@ export async function menunote(ctx) {
     
     
     return ctx.editMessageText(
-      `<b>Notes</b>\nPerintah yang Tersedia :\n\n<code>/save</code> <b>{noteName} {noteValue}</b> - simpan catatan baru atau perbarui catatan.\nUntuk <b>{noteValue}</b>bagian tersebut anda bisa menggantinya dengan membalas pesan.\n<code>/clear</code> <b>{noteName}</b> - hapus beberapa catatan\n<code>/clearall</code> - hapus semua catatan\n<code>/notes</code> - dapatkan daftar semua catatan`,
+      `Perintah yang Tersedia untuk <b>Notes</b>:\n\n<code>/save</code> <b>{noteName} {noteValue}</b> - simpan catatan baru atau perbarui catatan.\nUntuk <b>{noteValue}</b>bagian tersebut anda bisa menggantinya dengan membalas pesan.\n<code>/clear</code> <b>{noteName}</b> - hapus beberapa catatan\n<code>/clearall</code> - hapus semua catatan\n<code>/notes</code> - dapatkan daftar semua catatan`,
+      {
+        reply_markup: {
+          inline_keyboard: keyboard,
+        },
+        parse_mode: 'HTML',
+      }
+    )
+  } catch (error) {
+    return reportError(error, ctx)
+  }
+}
+
+
+export async function menupurge(ctx) {
+ 
+  try {
+    
+    let keyboard = [
+      [
+        {
+          text: '⬅ ᴋᴇᴍʙᴀʟɪ',
+          callback_data: 'helpmenu',
+          hide: true,
+        },
+      ],
+    ]
+    
+    
+    return ctx.editMessageText(
+      `Perintah yang Tersedia untuk <b>Purge</b>:\n\n<code>/purge</code> - Hapus Pesan.\n<i>Balas pesan dengan <code>/purge</code>. Bot akan menghapus pesan mulai dari pesan yang anda balas hingga pesan <code>/purge</code> terkirim. Untuk grup, hanya grup admin yang dapat menggunakan ini!</i>`,
+      {
+        reply_markup: {
+          inline_keyboard: keyboard,
+        },
+        parse_mode: 'HTML',
+      }
+    )
+  } catch (error) {
+    return reportError(error, ctx)
+  }
+}
+
+
+export async function menureport(ctx) {
+ 
+  try {
+    
+    let keyboard = [
+      [
+        {
+          text: '⬅ ᴋᴇᴍʙᴀʟɪ',
+          callback_data: 'helpmenu',
+          hide: true,
+        },
+      ],
+    ]
+    
+    
+    return ctx.editMessageText(
+      `Perintah yang Tersedia untuk <b>Report</b>:\n\nUntuk melaporkan seseorang ke admin Anda dapat menggunakan perintah <code>/report</code> atau <code>@admin</code> atau <code>@admins</code> dengan pengguna yang membalas yang akan melaporkan.`,
+      {
+        reply_markup: {
+          inline_keyboard: keyboard,
+        },
+        parse_mode: 'HTML',
+      }
+    )
+  } catch (error) {
+    return reportError(error, ctx)
+  }
+}
+
+export async function menuspecial(ctx) {
+ 
+  try {
+    
+    let keyboard = [
+      [
+        {
+          text: '⬅ ᴋᴇᴍʙᴀʟɪ',
+          callback_data: 'helpmenu',
+          hide: true,
+        },
+      ],
+    ]
+    
+    
+    return ctx.editMessageText(
+      `Perintah yang Tersedia untuk <b>Special Features</b>:\n\n<code>/ocr - ke ocr menggunakan api ruang ocr.\n<code>/ocrts {lang} - ke ocr menggunakan Tesseract.js\n<i>Gunakan perintah di atas dengan membalas foto.</i>\n\n<code>/cal</code> - dapatkan kalkulator sederhana\n<code>/tts</code> {isoLang} - mengubah teks menjadi ucapan.\n<i>Balas teks pesan dengan perintah ini!</i>\n\n<code>/kang</code> {emoji}| <code>/curi</code> {emoji} - mencuri atau menjadikan gambar sebagai stiker.\n<i>Balas pesan stiker atau foto dengan perintah ini!</i>\n\n<code>/tr</code> {isoLang} - menerjemahkan teks.\n\n<b>For isoLang bot only support language :</b>\n<code>["af","sq","am","ar","hy","az","eu","be","bn","bs","bg","ca","ceb","ny","zh-cn","zh-tw","co","hr","cs","da","nl","en","eo","et","tl","fi","fr","fy","gl","ka","de","el","gu","ht","ha","haw","iw","hi","hmn","hu","is","ig","id","ga","it","ja","jw","kn","kk","km","ko","ku","ky","lo","la","lv","lt","lb","mk","mg","ms","ml","mt","mi","mr","mn","my","ne","no","ps","fa","pl","pt","pa","ro","ru","sm","gd","sr","st","sn","sd","si","sk","sl","so","es","su","sw","sv","tg","ta","te","th","tr","uk","ur","uz","vi","cy","xh","yi","yo","zu"]</code>\n\n<b>For Tesseract.js Lang bot only support language :</b>\n<code>["afr","amh","ara","asm","aze","aze_cyrl","bel","ben","bod","bos","bul","cat","ceb","ces","chi_sim","chi_tra","chr","cym","dan","deu","dzo","ell","eng","enm","epo","est","eus","fas","fin","fra","frk","frm","gle","glg","grc","guj","hat","heb","hin","hrv","hun","iku","ind","isl","ita","ita_old","jav","jpn","kan","kat","kat_old","kaz","khm","kir","kor","kur","lao","lat","lav","lit","mal","mar","mkd","mlt","msa","mya","nep","nld","nor","ori","pan","pol","por","pus","ron","rus","san","sin","slk","slv","spa","spa_old","sqi","srp","srp_latn","swa","swe","syr","tam","tel","tgk","tgl","tha","tir","tur","uig","ukr","urd","uzb","uzb_cyrl","vie","yid"]</code>`,
       {
         reply_markup: {
           inline_keyboard: keyboard,
